75d0ede1271bdd83911d7b6e482ac54fd6c4b9a5
Michel Kabwe <mikael.tallbo@webexpress.se>
2023-03-02 18:35:16 +0100

    portfolio page modified , content fetched from kabwe.dev

---
+++ src/hooks/usePosts.tsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface Post {
+  id: number;
+  title: string;
+  featured_image: string;
+}
+
+export const usePosts = (): Post[] => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    axios
+      .get('https://public-api.wordpress.com/rest/v1.1/sites/kabwedev.wordpress.com/posts/')
+      .then((res) => {
+        setPosts(res.data.posts);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return posts;
+};
--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,63 +1,34 @@
-import React, { useState, useEffect } from 'react'
-import styles from '../../styles/Portfolio.module.css'
-import axios from 'axios'
+import React from "react";
+import styles from "../../styles/Portfolio.module.css";
+import { usePosts } from "../../hooks/usePosts";
 
+const Index: React.FC = () => {
+  const posts = usePosts();
 
-const Index: React.FC= () => {
-  const [posts, setPosts] = useState([])
-  const [postId, setPostId] = useState<any>([])
-
-   //.get(`https://africanent.se/wp-json/wp/v2/posts/posts/${postId}`)
-   //  .get('https://africanent.se/wp-json/wp/v2/posts/')
-   //.get(`https://africanent.se/wp-json/wp/v2/posts?_embed&per_page=3&orderby=date`)
-
-
-
-  useEffect(() => {
-    axios
-      .get(`https://africanent.se/wp-json/wp/v2/posts?_embed&per_page=3&orderby=date`)
-      .then((res) => {
-        setPosts(res.data)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }, [])
- 
   return (
     <div className={styles.portfolio_wrapper}>
-      <h1>portfolio page</h1>
-      { posts ? (
-          <ul>
-              {posts.map((item) => {
-                  console.log(
-                  item
-                  ,'img')
-                  return (
-                    (
-                        <li key={item.id} style={{color:'red' ,height: 'auto', width: '50%'}}>
-                            {item.title.rendered}
-                            {item.date}
-                            {item.featured_media_url}
-
-                            <img src={item.jetpack_featured_media_url} alt="Featured image" style={{color:'red' ,height: 'auto', width: '50%'}}/>
-                           {/*  {item.title.content.rendered}
-                           {item.featured_media_url}*/}
-
-                        
-                        </li>
-                    )
-                  )
-              })}
+      {posts.length ? (
+        <div className={styles.card_container}>
+          <ul className={styles.card_ul}>
+            {posts.map((item) => {
+              return (
+                <li key={item.id} className={styles.card_li}>
+                  <p className={styles.card_title}>{item.title}</p>
+                  <img
+                    src={item.featured_image}
+                    alt="Featured image"
+                    className={styles.card_img}
+                  />
+                </li>
+              );
+            })}
           </ul>
-      ): (
-          <p>Loading...</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
       )}
-
-
-   
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
